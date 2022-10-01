f7c068026387db3356bb4aa60f3ce04481888f2a
Kimsanlim <kimsanlim27@gmail.com>
2022-10-02 00:55:58 +0700

    Prevent access restricted page without logged in via URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?, :current_controller?
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,9 +14,5 @@ class ApplicationController < ActionController::Base
       flash[:notice] = "You must be logged in to perform that action."
       redirect_to login_path
     end
-  end
-
-  def current_controller?(names)
-    names.include?(params[:controller]) unless params[:controller].blank? || false
   end
 end
