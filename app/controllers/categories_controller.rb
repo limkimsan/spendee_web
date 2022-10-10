5addec6301f5b31276de349d693cbd09ea1ca662
Kimsanlim <kimsanlim27@gmail.com>
2022-10-11 00:36:38 +0700

    Create screen for create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,4 +6,19 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "New category is created!"
+      redirect_to categories_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :transaction_type, :order)
+  end
 end
