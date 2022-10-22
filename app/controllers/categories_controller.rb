d74d0db42371a4d00bb24920e4167c9c2aff2e9b
Kimsanlim <kimsanlim27@gmail.com>
2022-10-22 17:38:51 +0700

    Ability to edit category and add permission to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,19 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, only: [:edit]
+  before_action :set_category, only: [:edit, :update]
+
   def index
+    authorize User
     @categories = Category.all
   end
 
   def new
+    authorize User
     @category = Category.new
   end
 
   def create
+    authorize User
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = "New category is created!"
@@ -17,8 +23,31 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    authorize User
+    if @category.update(category_params)
+      flash[:success] = "The category was successfully updated"
+      redirect_to categories_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   private
   def category_params
     params.require(:category).permit(:name, :transaction_type, :order)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def require_admin
+    if !current_user.primary_admin? && !current_user.admin?
+      redirect_to root_path
+    end
   end
 end
