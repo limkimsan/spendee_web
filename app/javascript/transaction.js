f77d00e61ce76f07ee893fc8197b1485c0df3c71
Kimsanlim <kimsanlim27@gmail.com>
2022-11-14 00:23:43 +0700

    Fix record incorrecty transaction_type and category_id when create or edit transaction

--- app/javascript/transaction.js
+++ app/javascript/transaction.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-  $("#expense_category").hide();
-  $("#transaction_type").val('income');
-  $("#transaction_category").val($("#income_category").val())
+// $(document).ready(function(){
+//   $("#expense_category").hide();
+//   $("#transaction_type").val('income');
+//   $("#transaction_category").val($("#income_category").val())
 
-  $("#transaction_type_selection").change(function() {
-    const transactionType = $("#transaction_type_selection").val();
-    $("#transaction_type").val(transactionType);
-    if (transactionType == "expense") {
-      $("#expense_category").show()
-      $("#income_category").hide()
-      $("#transaction_category").val($("#expense_category").val())
-    }
-    else {
-      $("#expense_category").hide()
-      $("#income_category").show()
-      $("#transaction_category").val($("#income_category").val())
-    }
-  })
+//   $("#transaction_type_selection").change(function() {
+//     const transactionType = $("#transaction_type_selection").val();
+//     $("#transaction_type").val(transactionType);
+//     if (transactionType == "expense") {
+//       $("#expense_category").show()
+//       $("#income_category").hide()
+//       $("#transaction_category").val($("#expense_category").val())
+//     }
+//     else {
+//       $("#expense_category").hide()
+//       $("#income_category").show()
+//       $("#transaction_category").val($("#income_category").val())
+//     }
+//   })
 
-  $("#expense_category").change(() => {
-    $("#transaction_category").val($("#expense_category").val())
-  })
+//   $("#expense_category").change(() => {
+//     $("#transaction_category").val($("#expense_category").val())
+//   })
 
-  $("#income_category").change(() => {
-    $("#transaction_category").val($("#income_category").val())
-  })
-});
+//   $("#income_category").change(() => {
+//     $("#transaction_category").val($("#income_category").val())
+//   })
+// });
