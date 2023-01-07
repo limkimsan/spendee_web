f00fe0ad2e338972031ec81c2e100deebee22800
Kimsanlim <kimsanlim27@gmail.com>
2023-01-07 10:09:25 +0700

    Create API for getting the categories

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class CategoriesController < ApiController
+      def index
+        @categories = Category.all
+
+        render json: @categories
+      end
+    end
+  end
+end
