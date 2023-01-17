aa454c7a7aa710702f86f7b352d443c4576f8edf
Kimsanlim <kimsanlim27@gmail.com>
2023-01-18 00:26:47 +0700

    Add icon, icon_color, bg_color to category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id               :integer          not null, primary key
+#  name             :string
+#  transaction_type :integer
+#  order            :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  icon             :string
+#  icon_type        :string
+#  icon_color       :string
+#  bg_color         :string
+#
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name, :transaction_type, :order
+  attributes :id, :name, :transaction_type, :order, :icon, :icon_color, :bg_color
 end
